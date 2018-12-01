9fe761bfc26802ccd6fb96ff8649a3da8bb8d0b0
Moses <mocuevas91@gmail.com>
2018-12-01 12:10:44 -0800

    update UI, jpg in progress

--- Paint/src/paint/SaveAsJPG.java
+++ Paint/src/paint/SaveAsJPG.java
@@ -4,6 +4,7 @@ package saveasjpg;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import javax.imageio.ImageIO;
@@ -14,26 +15,66 @@ public class SaveAsJPG {
 	 */
     public static final int BLOCKSIZE = 8;
     
-	public static void main(String[] args) {
-		BufferedImage image = null;
+   private static final double[][] quantMatrix = {
+        { 16, 11, 10, 16, 24, 40, 51, 61},
+        { 12, 12, 14, 19, 26, 58, 60, 55},
+        { 14, 13, 16, 24, 40, 57, 69, 56},
+        { 14, 17, 22, 29, 51, 87, 80, 62},
+        { 18, 22, 37, 56, 68,109,103, 77},
+        { 24, 35, 55, 64, 81,104,113, 92},
+        { 49, 64, 78, 87,103,121,120,101},
+        { 72, 92, 95, 98,112,100,103, 99}
+    };
+    private static final double quality = 70;//1 is poor quality, 100 is high quality
+    
+    public class Number{
+        public byte value;
+        public byte occurance;
+
+        public Number() {
+            occurance = 0;
+        }
+        
+        
+    }
+    
+    public class Sortbyoccurance{
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        BufferedImage image = null;
+        
         try {
-            File pnginput = new File ("C:\\Users\\Moses Cuevas\\Pictures\\knowswhatsgood.png");
+            File pnginput = new File ("C:\\Users\\Moses Cuevas\\Pictures\\parts.png");
             image = ImageIO.read(pnginput);
-            
+
             int i,j;
-            
+            int oneDimCounter=0;
+
             //get information
             int height= image.getHeight();
             int width = image.getWidth();
-            
+
             int newWidth = width + (width % BLOCKSIZE);//make picture divisible by 8
             int newHeight= height + (height % BLOCKSIZE);
             //Color [][] colors = new Color [width][height];
-            int [][] lumina = new int [newWidth][newHeight];
-            int [][]DCTinput= new int [BLOCKSIZE][BLOCKSIZE];
-            int [][]DCTresult=new int [newWidth/BLOCKSIZE][newHeight/BLOCKSIZE];//after the dct is calculated
-            float red,green,blue;
+            double [][] lumina = new double [newWidth][newHeight];
+            double [][]dctInput= new double [BLOCKSIZE][BLOCKSIZE];
+            double [][]dctResult=new double [BLOCKSIZE][BLOCKSIZE];//after the dct is calculated
+            double [][]dctQuantized=new double[BLOCKSIZE][BLOCKSIZE];
             
+            Number []huffTable = new Number[256];
+            //initialize table
+            for(int k=0;k<256;k++){
+                huffTable[k].value = (byte) (k - 128);
+            }
+            
+            int []oneDimArray = new int [newWidth * newHeight];
+            byte[]outputbytes=new byte[newWidth*newHeight+3];//needs work
+            double red,green,blue;
+
             //change to YCbCr
             for(i=0; i<width; i++){
                 for(j=0; j<height; j++){
@@ -41,67 +82,197 @@ public class SaveAsJPG {
                     red = mycolor.getRed();
                     green = mycolor.getGreen();
                     blue = mycolor.getBlue();
-                    
-                    lumina[i][j] = (int)((.299 * red) + (.578 * green) + (.114 * blue));
+
+                    lumina[i][j] = (double)((.299 * red) + (.578 * green) + (.114 * blue) -127);//<-- -127 to produce values around zero
                 }
             }
+            
             //fill the rest with zero
             for(i = width; i < newWidth ;i++){
-                for(j = height; j<newHeight;j++){
+                for(j = 0; j < newHeight;j++){
+                    lumina[i][j] = 0;
+                }
+            }
+            for(i = 0; i < width ;i++){
+                for(j = height; j < newHeight;j++){
                     lumina[i][j] = 0;
                 }
             }
             
-            //divide into 8x8 blocks
-            for(i=0; i<i+BLOCKSIZE && i<newWidth; i=i+BLOCKSIZE){
-                for(j=0; j<j+BLOCKSIZE && j<newHeight; j=j+BLOCKSIZE){
-                    DCTinput[i%BLOCKSIZE][j%BLOCKSIZE] = lumina[i][j];
+            /*for(i=0; i<newWidth; i++){
+                for(j=0; j<newHeight; j++){
+                    System.out.print(lumina[i][j]+"\t");
                 }
-                DCTresult[i%BLOCKSIZE][j%BLOCKSIZE] = calcDCT(DCTinput);
+                System.out.print("\n");
+            }*/
+            
+            //divide into 8x8 blocks
+            int xstart,ystart;
+            int a,b;
+
+            for (i = 0; i < newWidth / BLOCKSIZE; i++) { 
+                for (j = 0; j < newHeight / BLOCKSIZE; j++) {
+
+                    xstart = i * BLOCKSIZE;
+                    ystart = j * BLOCKSIZE;
+                    // dividing into 8x8 blocks
+                    for (a = 0; a < BLOCKSIZE; a++) {
+                        for (b = 0; b < BLOCKSIZE; b++) {
+                            dctInput[a][b] = lumina[xstart + a][ystart + b];
+                        }
+                    }
+
+                    // Apply DCT to block
+                    dctResult = calcDCT(dctInput);
+                    
+                    // quantize the block
+                    dctQuantized = quantize(dctResult);
+
+                    //huffman encode - convert to 1D array and count frequency
+                    byte tempByte;
+                    for (a = 0; a < BLOCKSIZE; a++) {
+                        for (b = 0; b < BLOCKSIZE; b++) {
+                            tempByte = (byte)dctQuantized[a][b];
+                            oneDimArray[oneDimCounter] = tempByte;
+                            huffTable[(tempByte+128)].occurance++;//add one to the occurance
+                            oneDimCounter++;
+                        }
+                    }
+                    //sort by highest occurance to lowest
+                    for(int k=0;k<256;k++){
+                        ;
+                    }
+                    
+                    /*oneDimCounter=0;
+                    boolean upward = true;
+                    while(oneDimCounter != BLOCKSIZE*BLOCKSIZE){
+                        int f=0,g=0;
+                        oneDimArray[oneDimCounter++] = (byte)dctQuantized[f][g];
+                        if(upward){
+                            if(f==0){
+                                upward = false;
+                                g++;
+                            }
+                            else{
+                                f--;
+                                g++;
+                            }
+                        }
+                        else{
+                            if(g==0){
+                                upward = true;
+                                f++;
+                            }
+                            else{
+                                g--;
+                                f++;
+                            }
+                        }
+                    }*/
+                    
+                    /*for (a = 0; a < BLOCKSIZE; a++) {
+                        for (b = 0; b < BLOCKSIZE; b++) {
+                            reconstImage[xstart + a][ystart + b] =(int) dctQuantized[a][b];//fill the new reconstructed image from the 8x8 block (dctArray4)
+                        }
+                    }*/
+                    
+                }
             }
             
-            //apply DCT to each block
+            //for logic checking purposes
+            for (i = 0; i < newWidth*newHeight; i++) { 
+                System.out.print(oneDimArray[i]+"\t");
+                if((i+1)% newWidth == 0)
+                    System.out.print("\n");
+            }
             
-            //quantize each block
+
+            //write the image to a file, resource: http://web.cecs.pdx.edu/~harry/compilers/ASCIIChart.pdf
+            //http://www.file-recovery.com/jpg-signature-format.htm
+            outputbytes[0]= -1;  //ff StartOfImage
+            outputbytes[1]= -40; //d8 
+            outputbytes[2]= -1;  //ff Application Marker
+            outputbytes[3]= -32; //e0
+            outputbytes[4]= 16; //Length
+            outputbytes[5]= 16;
             
-            //huffman encode
+            outputbytes[6]= 74;//4a conforms to jfif
+            outputbytes[7]= 70;//46
+            outputbytes[8]= 73;//49
+            outputbytes[9]= 70;//46
+            outputbytes[10]= 0;//00
             
-            //write the image to a file
             
-            //ImageIO.write(image, "png",new File("C:\\Users\\Moses Cuevas\\Pictures\\newBird.jpg"));
+            outputbytes[40-1]= -1;//ff endOfImage
+            outputbytes[40]= -39;//d9
+            try( //ImageIO.write(image, "png",new File("C:\\Users\\Moses Cuevas\\Pictures\\newBird.jpg"));
+                FileOutputStream stream = new FileOutputStream(System.getProperty("user.dir")+"\\savedOutputFile.jpg")) {
+                stream.write(outputbytes);
+            }
+            //ImageIO.write(image, "jpg",new File("C:\\Users\\Moses Cuevas\\Pictures\\cheat.jpg"));
         }
         catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println("Done");
-	}
-	
-        private int calcDCT(int [][] dctarray){//garbage code that works like garbage  <----  <-----  <-----  <-----
-            int dctValue;
-            
-            for(int i=0;i<BLOCKSIZE;i++){
-                for(int j=0;j<BLOCKSIZE;j++){
-                    dctValue += value[j][i]
-                                    * Math.cos(((2 * i + 1) * u * Math.PI) / 16)
-                                    * Math.cos(((2 * j + 1) * v * Math.PI) / 16);
-                }
-            }
-            
-            for (i = 0; i < N; i++) {
-                for (j = 0; j < N; j++) {
-                    temp = 0.0;
-                    for (x = 0; x < N; x++) {
-                        for (y = 0; y < N; y++) {
-                            temp += Cosines[x][i] *
-                                Cosines[y][j] *
-                                Pixel[x][y];
-                        }
+    }
+
+    private static double[][] calcDCT(double [][] dctarray){
+
+        double[][] buffer = new double[BLOCKSIZE][BLOCKSIZE];
+
+        for (int u = 0; u < BLOCKSIZE; u++){
+            for (int v = 0; v < BLOCKSIZE; v++){
+                
+                buffer[u][v] = (double)(.25) * edgecase((int) u) * edgecase((int) v);//may need to cast to double
+                //System.out.print(buffer[u][v]+"\t");
+                double xysum = 0;
+                for (int x = 0; x < BLOCKSIZE; x++){
+                    for (int y = 0; y < BLOCKSIZE; y++){
+                        //System.out.print(dctarray[x][y]+"\t");
+                        xysum += dctarray[y][x]//flip for transpose
+                                      * Math.cos(((2 * x + 1) * u * Math.PI) / 16.0)
+                                      * Math.cos(((2 * y + 1) * v * Math.PI) / 16.0);
                     }
-                    temp *= sqrt(2 * N) * Coefficient[i][j];
-                    DCT[i][j] = INT_ROUND(temp);
+                   // System.out.print("\n");
                 }
+                buffer[u][v] *= xysum;
             }
-            
-            return dctValue;
+            //System.out.print("\n");
         }
+        //System.out.print("\n");
+        return (buffer);
+    }
+    
+    private static double edgecase(int index){// <-- <-- <-- don't forget to rename this!!! <-- <-- <--
+        double result;
+        if(index == 0){
+            result = (double) (1.0/Math.sqrt(2.0));
+        }
+        else{
+            result = (double) 1.0;
+        }
+        return result;
+    }
+    
+    private static double[][] quantize(double [][] dctarray){
+        int i, j;
+        double[][] quantum = new double[BLOCKSIZE][BLOCKSIZE];
+        for (i = 0; i < BLOCKSIZE; i++){
+            for (j = 0; j < BLOCKSIZE; j++){
+            	//quantum[i][j] = dctarray[i][j]/(1 + ((1 + i + j) * quality));
+                quantum[i][j] = dctarray[i][j]/Math.floor((S(quality)*quantMatrix[i][j]+50)/100);
+            }
+        }
+        return quantum;
+    }
+    
+    private static double S(double q){
+        if(q<50){
+            return 5000/q;
+        }
+        else{
+            return 200 - 2 * q;
+        }
+    }
 }
--- Paint/src/paint/paintfxmlController.java
+++ Paint/src/paint/paintfxmlController.java
@@ -1,22 +1,38 @@
 package paint;
 
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -24,6 +40,13 @@ import javafx.stage.Stage;
  */
 public class paintfxmlController implements Initializable {
 
+    //for testing
+    long timer1start, timer1end, test1time;
+    long timer2start, timer2end, test2time;
+    long timer3start, timer3end, test3time;
+    
+    double size = 6;
+    
     @FXML
     private ColorPicker colorpicker;
     
@@ -31,7 +54,14 @@ public class paintfxmlController implements Initializable {
     private TextField bsize;
     
     @FXML
+    private Button btn_draw, btn_more ,btn_less;
+    
+    @FXML
     private Canvas canvas;
+    
+    @FXML
+    private Rectangle recentcolor1,recentcolor2,recentcolor3,
+            recentcolor4,recentcolor5,recentcolor6;
     
     boolean toolSelected = false;
     
@@ -39,6 +69,15 @@ public class paintfxmlController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        recentcolor1.setFill(Color.GREEN);
+        recentcolor2.setFill(Color.YELLOW);
+        recentcolor3.setFill(Color.PURPLE);
+        recentcolor4.setFill(Color.ORANGE);
+        recentcolor5.setFill(Color.RED);
+        recentcolor6.setFill(Color.BLUE);
+        
+        colorpicker.setValue(Color.GREEN);
+        
         brushTool = canvas.getGraphicsContext2D();
         canvas.setOnMouseDragged(e -> {
             double size = Double.parseDouble(bsize.getText());
@@ -50,6 +89,32 @@ public class paintfxmlController implements Initializable {
                 brushTool.fillRoundRect(x, y, size, size, size, size);
             }
         });
+        
+        colorpicker.setOnMouseClicked(new EventHandler<MouseEvent>(){//needs work <-- <-- <-- <-- <--
+            @Override
+            public void handle(MouseEvent t){
+                recentcolor6.setFill(recentcolor5.getFill());
+                recentcolor5.setFill(recentcolor4.getFill());
+                recentcolor4.setFill(recentcolor3.getFill());
+                recentcolor3.setFill(recentcolor2.getFill());
+                recentcolor2.setFill(recentcolor1.getFill());
+                recentcolor1.setFill(colorpicker.getValue());
+            }
+        });
+        /*
+        btn_more.setOnMouseClicked(new EventHandler<MouseEvent>(){//needs work <-- <-- <-- <-- <--
+            @Override
+            public void handle(MouseEvent t){
+                size=size+1;
+            }
+        });
+        
+        btn_less.setOnMouseClicked(new EventHandler<MouseEvent>(){//needs work <-- <-- <-- <-- <--
+            @Override
+            public void handle(MouseEvent t){
+                size=size-1;
+            }
+        });*/
     }    
     
     @FXML
@@ -103,5 +168,96 @@ public class paintfxmlController implements Initializable {
     @FXML
     public void toolselected(ActionEvent e){
         toolSelected = true;   
+    }
+    
+    @FXML
+    public void saveAsJpeg(ActionEvent e){
+        /*BufferedImage bimage = new BufferedImage(canvas.getWidth(), canvas.getHeight(),BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = bimage.createGraphics();
+        canvas.paint(g);*/
+        SnapshotParameters sp = new SnapshotParameters();
+        sp.setFill(Color.WHITE);//can set to TRANSPARENT
+        
+        WritableImage wimage = new WritableImage((int)canvas.getWidth(), (int)canvas.getHeight());
+        canvas.snapshot(sp,wimage);
+        
+        File file = new File("testFromJavaPaint.png");
+        //BufferedImage bimage = (SwingFXUtils.fromFXImage(img, null))
+        
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(wimage, null), "png", file);//remove this
+        } catch (IOException ex) {
+            // TODO: handle exception here
+        }
+    }
+    
+    @FXML
+    public void test(ActionEvent e){//for testing purposes only - can be removed later
+        Button btnTest1 = new Button();
+        btnTest1.setText("Test 1");
+        
+        Button btnTest2 = new Button();
+        btnTest2.setText("Test 2");
+        
+        Button btnTest3 = new Button();
+        btnTest3.setText("Test 3");
+        
+        Stage createStage = new Stage();
+        AnchorPane root = new AnchorPane();
+        root.setPrefWidth(200);
+        root.setPrefHeight(200);
+        
+        VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setLayoutX(70);
+        vbox.setAlignment(Pos.CENTER);
+        vbox.getChildren().addAll(btnTest1, btnTest2, btnTest3);
+        
+        root.getChildren().add(vbox);
+        
+        Scene CanvasScene = new Scene (root);
+        createStage.setTitle("Create Canvas");
+        createStage.setScene(CanvasScene);
+        createStage.show();
+        
+        btnTest1.setOnAction(new EventHandler<ActionEvent>(){//draw a 1 in diameter circle and fill it
+            @Override
+            public void handle (ActionEvent event){
+                createStage.close();
+                timer1start = System.currentTimeMillis();
+                System.out.println("Test 1 start: "+ timer1start);
+                
+                btn_draw.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+                    timer1end = System.currentTimeMillis();
+                    System.out.println("  Test 1 end: "+ timer1end);
+                    test1time = timer1end - timer1start;
+                    System.out.println("Test 1 time: "+ test1time);
+                });
+            }
+        });
+        
+        btnTest2.setOnAction(new EventHandler<ActionEvent>(){//draw a line using bezier tool
+            @Override
+            public void handle (ActionEvent event){
+                
+                createStage.close();
+                timer2start = System.currentTimeMillis();
+                
+                timer2end = System.currentTimeMillis();
+                test2time = timer2end - timer2start;
+            }
+        });
+        
+        btnTest3.setOnAction(new EventHandler<ActionEvent>(){//select a color and draw a 1 inch square
+            @Override
+            public void handle (ActionEvent event){
+                
+                createStage.close();
+                timer1start = System.currentTimeMillis();
+                
+                timer3end = System.currentTimeMillis();
+                test3time = timer3end - timer3start;
+            }
+        });
     }
 }
